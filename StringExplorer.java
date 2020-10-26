39fbff8a3269f0c8f3ea73599ce344ff8a76c0fc
Sydney Lohman <slohman0149@myduneland.org>
2020-10-26 09:26:27 -0500

    Editing of StringExplorer

--- StringExplorer.java
+++ StringExplorer.java
@@ -7,21 +7,31 @@
 public class StringExplorer
 {
 
-	public static void main(String[] args)
-	{
-		String sample = "The quick brown fox jumped over the lazy dog.";
-		
-		//  Demonstrate the indexOf method.
-		int position = sample.indexOf("quick");
-		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
-		//  Demonstrate the toLowerCase method.
-		String lowerCase = sample.toLowerCase();
-		System.out.println ("sample.toLowerCase() = " + lowerCase);
-		System.out.println ("After toLowerCase(), sample = " + sample);
-		
-		//  Try other methods here:
+    public static void main(String[] args)
+    {
+        String sample = "The quick brown fox jumped over the lazy dog.";
+        
+        //  Demonstrate the indexOf method.
+        int position = sample.indexOf("quick");
+        System.out.println ("sample.indexOf(\"quick\") = " + position);
+        int notFoundPsn = sample.indexOf("slow");
+        System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+        int position2 = sample.indexOf("jumped");
+        System.out.println("sample.indexOf(\"jumped\") = " + position2);
+        int notFoundPsn2 = sample.indexOf("jump");
+        System.out.println("sample.indexOf(\"jump\") = " + notFoundPsn2);
+        int position3 = sample.indexOf("brown");
+        System.out.println("sample.indexOf(\"brown\") = " + position3);
+        int notFoundPsn3 = sample.indexOf("own");
+        System.out.println("sample.indexOf(\"own\") = " + notFoundPsn3);
+        
+        //  Demonstrate the toLowerCase method.
+        String lowerCase = sample.toLowerCase();
+        System.out.println ("sample.toLowerCase() = " + lowerCase);
+        System.out.println ("After toLowerCase(), sample = " + sample);
+        
+        //  Edited by Sydney Lohman
 
-	}
+    }
 
 }
